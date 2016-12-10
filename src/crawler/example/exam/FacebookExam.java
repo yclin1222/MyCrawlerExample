b3712d2d035eb9df5a6657538909085878ebc84a
yuchieh Lin <winds15986@gmail.com>
2016-12-10 16:30:40 +0800

    卡提諾狂新聞練習

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -20,13 +20,13 @@ import org.jsoup.select.Elements;
 public class FacebookExam {
 	
 	public static void main(String[] args) {
-		
+		//狂新聞 按讚數
 		// 遠端資料路徑
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/crazyck101/feed?fields=id%2Clink%2Cmessage%2Ccreated_time%2Creactions.limit(0).summary(total_count)&since=1480849200&until=1480856400"
+				+ "&access_token=EAACEdEose0cBAMpNLBaImZBYWs2ee6kv4woBZC7S9eiUDZAGUdBuiDhVA8ppuaaZBqM8AKH5QvSu7a17Wa5sHzJll8xEwelIPZClBrJnH4EpnzMwhtG7hcptZCXlh8NO4gIWoUDYcCvNilJmV5zM2s7VkIRCfROYCTvAxNoL6njAZDZD";
 
 
 		Elements elems =
@@ -34,17 +34,17 @@ public class FacebookExam {
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+		String output = "id ,reactions";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions= data.select("reactions summary total_count").text();
 
 
-			output += id + "," + reactions + "\n";
+			output += id + ",  " + reactions + "\n";
 		}
 
 		System.out.println( output );
