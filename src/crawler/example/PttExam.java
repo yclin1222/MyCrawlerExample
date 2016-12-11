cbd5150a5844083d9df99926c00a04c54ec36dcb
yuchieh Lin <winds15986@gmail.com>
2016-12-11 11:12:37 +0800

    ptt練習

---
+++ src/crawler/example/PttExam.java
@@ -0,0 +1,42 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PttExam {
+	// commit test
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481379264.A.560.html";
+
+		System.out.println(
+				CrawlerPack.start()
+				
+				// 參數設定
+			    .addCookie("over18","1")	// 設定cookie
+				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+				
+				// 選擇資料格式 (三選一)
+				//.getFromJson(uri)
+			    .getFromHtml(uri)
+			    //.getFromXml(uri)
+			    
+			    // 這兒開始是 Jsoup Document 物件操作
+			    //.select("div:matches(push)\\ ")
+			    
+		);
+	}
+}
