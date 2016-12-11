b6202dae4c5b505e4d746daf15143518c322c6d5
yuchieh Lin <winds15986@gmail.com>
2016-12-11 12:06:58 +0800

    ptt練習

--- src/crawler/example/PttExam.java
+++ src/crawler/example/PttExam.java
@@ -2,6 +2,8 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +22,25 @@ public class PttExam {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481379264.A.560.html";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481371811.A.B97.html";
 
-		System.out.println(
-				CrawlerPack.start()
-				
+		Document jsoup = CrawlerPack.start()
+
 				// 參數設定
 			    .addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
 				
 				// 選擇資料格式 (三選一)
 				//.getFromJson(uri)
-			    .getFromHtml(uri)
+			    .getFromHtml(uri);
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
-			    //.select("div:matches(push)\\ ")
-			    
-		);
+
+                for(Element elem : jsoup.select("#main-content *")){
+
+				   elem.remove();
+        }
+			   System.out.println(jsoup.select("#main-content").text());
 	}
 }
